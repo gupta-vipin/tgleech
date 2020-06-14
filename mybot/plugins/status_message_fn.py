476097d548573b271e51b9b934f097d64214fc99
Vipin Gupta <vipin0900@gmail.com>
2020-06-14 07:57:00 +0530

    Removed admin check

--- mybot/plugins/status_message_fn.py
+++ mybot/plugins/status_message_fn.py
@@ -20,13 +20,11 @@ from mybot import (
 )
 
 
-from mybot.helper_funcs.admin_check import AdminCheck
 from mybot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
 from mybot.helper_funcs.upload_to_tg import upload_to_tg
 
 
 async def status_message_f(client, message):
-    if await AdminCheck(client, message.chat.id, message.from_user.id):
         aria_i_p = await aria_start()
         # Show All Downloads
         downloads = aria_i_p.get_downloads()
@@ -94,55 +92,11 @@ async def cancel_message_f(client, message):
         await message.delete()
 
 
-async def exec_message_f(client, message):
-    if await AdminCheck(client, message.chat.id, message.from_user.id):
-        DELAY_BETWEEN_EDITS = 0.3
-        PROCESS_RUN_TIME = 100
-        cmd = message.text.split(" ", maxsplit=1)[1]
-
-        reply_to_id = message.message_id
-        if message.reply_to_message:
-            reply_to_id = message.reply_to_message.message_id
-
-        start_time = time.time() + PROCESS_RUN_TIME
-        process = await asyncio.create_subprocess_shell(
-            cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await process.communicate()
-        e = stderr.decode()
-        if not e:
-            e = "No Error"
-        o = stdout.decode()
-        if not o:
-            o = "No Output"
-        else:
-            _o = o.split("\n")
-            o = "`\n".join(_o)
-        OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
-
-        if len(OUTPUT) > MAX_MESSAGE_LENGTH:
-            with open("exec.text", "w+", encoding="utf8") as out_file:
-                out_file.write(str(OUTPUT))
-            await client.send_document(
-                chat_id=message.chat.id,
-                document="exec.text",
-                caption=cmd,
-                disable_notification=True,
-                reply_to_message_id=reply_to_id
-            )
-            os.remove("exec.text")
-            await message.delete()
-        else:
-            await message.reply_text(OUTPUT)
-
 
 async def upload_document_f(client, message):
     imsegd = await message.reply_text(
         "processing ..."
     )
-    if await AdminCheck(client, message.chat.id, message.from_user.id):
         if " " in message.text:
             recvd_command, local_file_name = message.text.split(" ", 1)
             recvd_response = await upload_to_tg(
